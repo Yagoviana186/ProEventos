44b08dfd9a1ec9a7510e846c20b8102dc1a9fcd1
yago viana <Yago.viana.melo12@gmail.com>
2022-02-24 17:23:22 -0300

    first commit

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api[controller]")]
+    public class EventosController : ControllerBase
+    {
+            
+    public IEnumerable<Evento> _evento = new Evento[] {
+        new Evento() {
+        EventoId = 1,
+        Tema = "Angular 11 e .NET 5",
+        Local = "Belo Horizonte",
+        Lote = "1° Lote",
+        QtdPessoas = 250,
+        DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+        ImagemURL = "foto.png"
+                    
+        },
+        new Evento() {
+        EventoId = 2,
+        Tema = "Angular e Suas Novidades",
+        Local = "Sao Paulo",
+        Lote = "2° Lote",
+        QtdPessoas = 350,
+        DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+        ImagemURL = "foto1.png"
+                    
+            }
+        };
+
+        public DataContext Context { get; }
+
+        public EventosController(DataContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+           return _evento;
+           return new Evento[] {
+            new Evento() {
+            EventoId = 1,
+            Tema = "Angular 11 e .NET 5",
+            Local = "Belo Horizonte",
+            Lote = "1° Lote",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString(),
+            ImagemURL = "foto.png"
+            
+            },
+             new Evento() {
+            EventoId = 1,
+            Tema = "Angular 11 e .NET 5",
+            Local = "Belo Horizonte",
+            Lote = "1° Lote",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString(),
+            ImagemURL = "foto.png"
+            
+            }
+        };
+    }
+
+        
+      
+        [HttpGet("{id}")]
+
+        public Evento GetById(int id)
+        {
+            return _evento.FirstOrDefault
+            (evento => evento.EventoId == id);
+        }
+        
+        
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+        
+        [HttpPut ("{id}")]
+
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete com id = {id}";
+        
+        
+        }
+    }
+}
---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
+import { Subscriber } from 'rxjs';
+
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.geteventos();
+  }
+
+  public geteventos(): void{
+
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      Response => this.eventos = Response,
+      error   => console.log(error)
+   );
+
+
+}
+
+
+}
